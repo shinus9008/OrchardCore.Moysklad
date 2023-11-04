b1d1f2b41fe78cc1ee32341b7855fe70afc27e95
shinus9008 <shinus9008@gmail.com>
2023-11-04 19:48:01 +0500

    add class for define content type

---
+++ src/Modules/OrchardCore.Moysklad/Constants/DefineContentType.cs
@@ -0,0 +1,7 @@
+﻿namespace OrchardCore.Moysklad.Constants
+{
+    public static class DefineContentType
+    {
+        public const string MoyskladAssortmentQuery = nameof(MoyskladAssortmentQuery);
+    }
+}
