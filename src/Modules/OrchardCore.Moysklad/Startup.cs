ae3301daa315fd846ca0d959ae386b2571490452
shinus9008 <shinus9008@gmail.com>
2023-11-04 19:43:13 +0500

    add Route map

--- src/Modules/OrchardCore.Moysklad/Startup.cs
+++ src/Modules/OrchardCore.Moysklad/Startup.cs
@@ -3,19 +3,21 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
+using OrchardCore.Admin;
 using OrchardCore.ContentManagement;
 using OrchardCore.ContentManagement.Display.ContentDisplay;
-using OrchardCore.ContentManagement.Handlers;
 using OrchardCore.ContentTypes.Editors;
 using OrchardCore.Data.Migration;
 using OrchardCore.DisplayManagement.Handlers;
 using OrchardCore.Modules;
 using OrchardCore.Moysklad.Configuration;
+using OrchardCore.Moysklad.Controllers;
 using OrchardCore.Moysklad.Drivers;
 using OrchardCore.Moysklad.Handlers;
 using OrchardCore.Moysklad.Models;
 using OrchardCore.Moysklad.Settings;
 using OrchardCore.Moysklad.ViewModels;
+using OrchardCore.Mvc.Core.Utilities;
 using OrchardCore.Navigation;
 using OrchardCore.Security.Permissions;
 using OrchardCore.Settings;
@@ -24,6 +26,13 @@ namespace OrchardCore.Moysklad
 {
     public class Startup : StartupBase
     {
+        private readonly AdminOptions _adminOptions;
+
+        public Startup(IOptions<AdminOptions> adminOptions)
+        {
+            _adminOptions = adminOptions.Value;
+        }
+
         public override void ConfigureServices(IServiceCollection services)
         {
             //
@@ -57,12 +66,25 @@ namespace OrchardCore.Moysklad
 
         public override void Configure(IApplicationBuilder builder, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
         {
+            
+
             routes.MapAreaControllerRoute(
                 name: "Home",
                 areaName: "OrchardCore.Moysklad",
                 pattern: "Home/Index",
                 defaults: new { controller = "Home", action = "Index" }
             );
+
+
+
+            var assortmentControllerName = typeof(MoyskladAssortmentController).ControllerName();
+
+
+            routes.MapAreaControllerRoute(
+                name: "CreateQuery",
+                areaName: "OrchardCore.Moysklad",
+                pattern: _adminOptions.AdminUrlPrefix + "/MoyskladAssortment/CreateQuery/{hRef}",
+                defaults: new { controller = assortmentControllerName, action = nameof(MoyskladAssortmentController.CreateQuery) });
         }
     }
 }
