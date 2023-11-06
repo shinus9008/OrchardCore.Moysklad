f2eb66376c4100eb8e2162dfd34a3f0bf5ce36d4
shinus9008 <shinus9008@gmail.com>
2023-11-06 16:24:11 +0500

    add indexation (not registered)

---
+++ src/Modules/OrchardCore.Moysklad/Indexes/MoyskladAssortmentQueryPartIndex.cs
@@ -0,0 +1,9 @@
+﻿using YesSql.Indexes;
+
+namespace OrchardCore.Moysklad.Indexes
+{
+    public class MoyskladAssortmentQueryPartIndex : MapIndex
+    {
+        public required string ContentItemId { get; init; }
+    }
+}
---
+++ src/Modules/OrchardCore.Moysklad/Indexes/MoyskladAssortmentQueryPartIndexProvider.cs
@@ -0,0 +1,22 @@
+﻿using OrchardCore.ContentManagement;
+using OrchardCore.Moysklad.Models;
+using YesSql.Indexes;
+
+namespace OrchardCore.Moysklad.Indexes
+{
+    public class MoyskladAssortmentQueryPartIndexProvider : IndexProvider<ContentItem>
+    {
+        public override void Describe(DescribeContext<ContentItem> context) => context.For<MoyskladAssortmentQueryPartIndex>().Map(contentItem =>
+        {
+            // Если элемент контента не содержит QueryPart, то индексацию не производим!
+            var personPart = contentItem.As<MoyskladAssortmentQueryPart>();
+            if (personPart == null)
+                return null!;
+
+            return new MoyskladAssortmentQueryPartIndex
+            {
+                ContentItemId = contentItem.ContentItemId
+            };
+        });
+    }
+}
