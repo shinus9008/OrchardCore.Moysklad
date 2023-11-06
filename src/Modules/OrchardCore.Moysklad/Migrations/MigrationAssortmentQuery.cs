97f82f8cbb1ac04deace84972c9cedc21c0e5eb5
shinus9008 <shinus9008@gmail.com>
2023-11-06 15:42:05 +0500

    add migration

---
+++ src/Modules/OrchardCore.Moysklad/Migrations/MigrationAssortmentQuery.cs
@@ -0,0 +1,30 @@
+﻿using OrchardCore.ContentManagement.Metadata;
+using OrchardCore.ContentManagement.Metadata.Settings;
+using OrchardCore.Data.Migration;
+using OrchardCore.Moysklad.Constants;
+using OrchardCore.Moysklad.Models;
+
+namespace OrchardCore.Moysklad.Migrations
+{
+    public class MigrationAssortmentQuery : DataMigration
+    {
+        IContentDefinitionManager _contentDefinitionManager;
+
+        public MigrationAssortmentQuery(IContentDefinitionManager contentDefinitionManager)
+        {
+            _contentDefinitionManager = contentDefinitionManager;
+        }
+
+        public int Create()
+        {
+            _contentDefinitionManager.AlterTypeDefinition(DefineContentType.MoyskladAssortmentQuery, type => type
+                .Securable()
+                .DisplayedAs("Assortment Query")
+                .WithDescription("Moysklad Assortment Query")
+                .WithPart(DefineContentPart.TitlePart)
+                .WithPart(nameof(MoyskladAssortmentQueryPart)));
+
+            return 1;
+        }
+    }
+}
---
+++ src/Modules/OrchardCore.Moysklad/Migrations/MigrationAssortmentQueryPart.cs
@@ -0,0 +1,28 @@
+﻿using OrchardCore.ContentManagement.Metadata;
+using OrchardCore.ContentManagement.Metadata.Settings;
+using OrchardCore.Data.Migration;
+using OrchardCore.Moysklad.Constants;
+using OrchardCore.Moysklad.Models;
+
+namespace OrchardCore.Moysklad.Migrations
+{
+    public class MigrationAssortmentQueryPart : DataMigration
+    {
+        IContentDefinitionManager _contentDefinitionManager;
+
+        public MigrationAssortmentQueryPart(IContentDefinitionManager contentDefinitionManager)
+        {
+            _contentDefinitionManager = contentDefinitionManager;
+        }
+
+        public int Create()
+        {
+            _contentDefinitionManager.AlterPartDefinition(nameof(MoyskladAssortmentQueryPart), builder => builder
+                .Attachable()
+                .WithDisplayName("Assortment Query Part")
+                .WithDescription("Moysklad Assortment Query Part"));
+
+            return 1;
+        }
+    }
+}
