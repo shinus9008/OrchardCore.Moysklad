11964e573d375f64e37c39aba624180d8bf57a8e
shinus9008 <shinus9008@gmail.com>
2023-11-06 15:42:24 +0500

    add query part

---
+++ src/Modules/OrchardCore.Moysklad/Models/MoyskladAssortmentQueryPart.cs
@@ -0,0 +1,9 @@
+﻿using OrchardCore.ContentManagement;
+
+namespace OrchardCore.Moysklad.Models
+{
+    public class MoyskladAssortmentQueryPart : ContentPart
+    {
+        public string? ProductFolder { get; set; }
+    }
+}
