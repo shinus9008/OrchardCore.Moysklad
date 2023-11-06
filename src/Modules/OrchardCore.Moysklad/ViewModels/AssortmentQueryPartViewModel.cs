b8ceba1620cb283738d5bee6290612196ff588a1
shinus9008 <shinus9008@gmail.com>
2023-11-06 15:49:12 +0500

    add asortiment view model

---
+++ src/Modules/OrchardCore.Moysklad/ViewModels/AssortmentQueryPartViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using OrchardCore.ContentManagement;
+using OrchardCore.Moysklad.Models;
+
+namespace OrchardCore.Moysklad.ViewModels
+{
+    public class AssortmentQueryPartViewModel
+    {
+        //TODO: Добавить!
+
+        [BindNever]
+        public ContentItem ContentItem { get; internal set; }
+
+        [BindNever]
+        public MoyskladAssortmentQueryPart AssortmentQueryPart { get; internal set; }
+    }
+}
